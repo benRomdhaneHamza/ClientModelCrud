9bf56a61b353ebaa8ab03030e5801cce19689d01
benRomdhaneHamza <hamzabnromdhane@gmail.com>
2025-11-26 15:44:24 +0000

    fixed test config to run mvn clean

--- src/test/java/com/hamzabnr/ClientModelCrud/ClientModelCrudApplicationTests.java
+++ src/test/java/com/hamzabnr/ClientModelCrud/ClientModelCrudApplicationTests.java
@@ -3,7 +3,9 @@ package com.hamzabnr.ClientModelCrud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.config.name=application-test"
+})
 class ClientModelCrudApplicationTests {
 
 	@Test
